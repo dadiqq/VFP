8ba9275598b3d03daeb68efa430facfb3f4b7d7c
lmq-pc <gylimingqi@163.com>
2017-01-01 14:35:25 +0800

    2017-01-01 14:35:25  M  gitclone.sh

--- gitclone.sh
+++ gitclone.sh
@@ -1,6 +1,11 @@
-git clone  git@github.com:dadiqq/nework-git.git
+
+cp -r VFP  VFP`date +%Y%m%d%H:%M:%S`
+rm -rf VFP
+git clone  https://github.com/dadiqq/VFP.git
+##git clone  git@github.com:dadiqq/nework-git.git
 ##git checkout -b dev  --创建并切换到 dev分支    git branch  ---查看分支
 ##git checkout -b dev  --创建并切换到 dev分支  ##  git branch  ---查看分支
 #git checkout master --切换分支
 #git merge dev  --合并到当前分支
 #git branch -d dev   --删除分支
+##copy VFP  VFPdate +%Y%m%d%H:%M:%S
